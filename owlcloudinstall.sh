fb6d80538767afebf5ddb5648594d93bd2eab1f9
Kupreev Denis <kupreev.d.v@gmail.com>
2016-07-23 23:03:00 +0300

    Add YES after command

--- owlcloudinstall.sh
+++ owlcloudinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum update
-rpm --import https://download.owncloud.org/download/repositories/9.0/CentOS_6_SCL_PHP54/repodata/repomd.xml.key
-wget http://download.owncloud.org/download/repositories/9.0/CentOS_6_SCL_PHP54/ce:9.0.repo -O /etc/yum.repos.d/ce:9.0.repo
-yum clean expire-cache
-yum install owncloud
-yum install owncloud-filesy
+yum update -y
+rpm --import https://download.owncloud.org/download/repositories/9.0/CentOS_6_SCL_PHP54/repodata/repomd.xml.key -y
+wget http://download.owncloud.org/download/repositories/9.0/CentOS_6_SCL_PHP54/ce:9.0.repo -O /etc/yum.repos.d/ce:9.0.repo -y
+yum clean expire-cache -y
+yum install owncloud -y
+yum install owncloud-filesy -y
